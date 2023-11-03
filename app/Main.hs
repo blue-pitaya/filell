be25f20179fe0d6a00bf07d27984a20d4c86667f
blue-pitaya <blue.pitaya@pm.me>
2023-11-03 22:56:10 +0100

    showing contents of currently selected dir

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use newtype instead of data" #-}
 
 module Main (main) where
 
@@ -10,30 +11,38 @@ import Brick.Main as M
 import qualified Brick.Types as T
 import qualified Brick.Widgets.List as L
 import Control.Monad (void)
+-- import Lens.Micro.Mtl
+
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Data.Vector as VE
 import qualified Graphics.Vty as V
 import Lens.Micro ((^.))
--- import Lens.Micro.Mtl
 import Lens.Micro.TH
 import System.Directory
 import System.Environment
 import System.FilePath
+
+data WidgetId = ParentDirListKind | CurrentDirListKind | ChildDirListId deriving (Eq, Ord, Show)
 
 data DirItem = DirItem {dirItemIsDir :: Bool, dirItemName :: String, dirItemFullPath :: String}
 
 instance Show DirItem where
   show item = (if dirItemIsDir item then "d " else "- ") <> dirItemName item
 
-data AppState = AppState {_currentList :: L.List () DirItem}
+data AppState = AppState
+  { _currentList :: L.List WidgetId DirItem,
+    _parentDirList :: L.List WidgetId DirItem,
+    _childDirList :: L.List WidgetId DirItem
+  }
 
 makeLenses ''AppState
 
 rootDirFromArgs :: [String] -> String
-rootDirFromArgs [] = "/home/kodus/testi"
+rootDirFromArgs [] = "/home/kodus/testi/bulma"
 rootDirFromArgs (arg : _) = arg
 
-itemsList :: VE.Vector a -> L.List () a
-itemsList xs = L.list () xs 3
+makeList :: WidgetId -> VE.Vector a -> L.List WidgetId a
+makeList kind xs = L.list kind xs 3
 
 fromPath :: FilePath -> FilePath -> IO DirItem
 fromPath root filePath = do
@@ -47,41 +56,68 @@ getItems path = do
   items <- mapM (fromPath path) contentPahts
   return $ VE.fromList items
 
+-- TODO: rootDir must be full path!
 createAppState :: FilePath -> IO AppState
 createAppState rootDir = do
-  items <- getItems rootDir
-  return AppState {_currentList = itemsList items}
+  currentDirItems <- getItems rootDir
+  parentDirItems <- getItems (takeDirectory rootDir)
+  let currentList' = makeList CurrentDirListKind currentDirItems
+  return
+    AppState
+      { _currentList = currentList',
+        _parentDirList = makeList ParentDirListKind parentDirItems,
+        _childDirList = makeList ChildDirListId VE.empty
+      }
 
-renderDirItem :: Bool -> DirItem -> Widget ()
-renderDirItem isSelected item = str $ selectedArrow <> show item
-  where
-    selectedArrow = if isSelected then "-> " else ""
+renderDirItem :: Bool -> DirItem -> Widget a
+renderDirItem _ item = str $ show item
 
-renderApp :: AppState -> Widget ()
-renderApp appState = L.renderList renderDirItem True (appState ^. currentList)
-
-customAttr :: A.AttrName
-customAttr = L.listSelectedAttr <> A.attrName "custom"
+renderApp :: AppState -> [Widget WidgetId]
+renderApp appState =
+  [ L.renderList renderDirItem False (appState ^. parentDirList)
+      <+> L.renderList renderDirItem True (appState ^. currentList)
+      <+> L.renderList renderDirItem False (appState ^. childDirList)
+  ]
 
 theMap :: A.AttrMap
 theMap =
   A.attrMap
     V.defAttr
     [ (L.listAttr, V.white `on` V.black),
-      (L.listSelectedAttr, V.white `on` V.blue),
-      (customAttr, fg V.red)
+      (L.listSelectedAttr, V.white `on` V.blue)
     ]
 
-appEvent :: T.BrickEvent () e -> T.EventM () AppState ()
+updateChildrenOnSelectedItem :: DirItem -> IO (L.List WidgetId DirItem)
+updateChildrenOnSelectedItem dirItem =
+  if dirItemIsDir dirItem
+    then do
+      items <- getItems (dirItemFullPath dirItem)
+      return (makeList ChildDirListId items)
+    else return (makeList ChildDirListId VE.empty)
+
+updateListsAfterSelectedCHanged :: AppState -> IO AppState
+updateListsAfterSelectedCHanged s = do
+  let currList = s ^. currentList
+  let selected = fmap snd (L.listSelectedElement currList)
+  case selected of
+    Just item -> do
+      nextList <- updateChildrenOnSelectedItem item
+      return (s {_childDirList = nextList})
+    _ -> return s
+
+appEvent :: T.BrickEvent WidgetId e -> T.EventM WidgetId AppState ()
 appEvent (T.VtyEvent e) = case e of
   V.EvKey exitKey [] | exitKey `elem` [V.KEsc, V.KChar 'q'] -> M.halt
-  ev -> T.zoom currentList $ L.handleListEvent ev
+  ev -> do
+    _ <- T.zoom currentList (L.handleListEventVi L.handleListEvent ev)
+    s <- get
+    liftIO (updateListsAfterSelectedCHanged s) >>= put
 appEvent _ = return ()
 
-theApp :: M.App AppState e ()
+theApp :: M.App AppState e WidgetId
 theApp =
   M.App
-    { M.appDraw = \state -> [renderApp state],
+    { M.appDraw = renderApp,
       M.appChooseCursor = M.showFirstCursor,
       M.appHandleEvent = appEvent,
       M.appStartEvent = return (),
